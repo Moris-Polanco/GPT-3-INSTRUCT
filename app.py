1f8d6e09278472fbf56c980a7fcc595e74bb8d8a
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2022-10-29 19:25:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import streamlit as st
 openai.api_key = st.secrets["SECRET_KEY"]
 
 
-st.title('GPT-3 ')
+st.title('GPT-3 Instruct')
 
 st.text('This GPT-3 model is the Davinci-engine')
 prompt_text = st.text_input(label="Add here phrase, which you want to be completed", value="Add here few words")
-response = openai.Completion.create(engine="davinci", prompt=prompt_text, max_tokens=5)
-st.text('Remaining phrase:')
+response = openai.Completion.create(engine="davinci", prompt=Escriba un artículo para blog sobre el tema indicado, max_tokens=3000)
+st.text('Artículo:')
 st.text(response["choices"][0]["text"])
