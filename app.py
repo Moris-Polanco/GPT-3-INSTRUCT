5565f53eed4920c0e1499635d77d67b99ee7cb50
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2022-10-29 20:06:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ openai.api_key = st.secrets["SECRET_KEY"]
 st.title('GPT-3 Instruct')
 
 st.text('This GPT-3 model is the Davinci-engine')
-prompt_text = st.text_input(label="Add here phrase, which you want to be completed", value="Add here few words")
+prompt_text = st.text_input(label="Add here phrase, which you want to be completed", value="Escriba un caso de ética, con las palabras clave que se proponen")
 response = openai.Completion.create(engine="text-davinci-002", prompt=prompt_text, max_tokens=3000)
 st.text('Remaining phrase:')
 st.markdown(response["choices"][0]["text"])
