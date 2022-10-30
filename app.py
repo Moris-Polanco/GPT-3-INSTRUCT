6880db9ac201bf3ebc64a4a83542f51fbf44c967
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2022-10-29 19:41:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ openai.api_key = st.secrets["SECRET_KEY"]
 st.title('GPT-3 Instruct')
 
 st.text('This GPT-3 model is the Davinci-engine')
-prompt_text = st.text_input(label="Add here phrase, which you want to be completed", value="Add here few words")
-response = openai.Completion(engine="text-davinci-002", prompt="Escriba un artículo para blog sobre el tema indicado", max_tokens=3000)
+prompt_text = st.text_input(label="Instruya a Davinci", value="Dígale qué necesita escribir")
+response = openai.Completion(engine="text-davinci-002", prompt=promp_text, max_tokens=3000)
 st.text('Artículo:')
-st.text('response')
+st.text((response["choices"][0]["text"]))
