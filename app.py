236b353f9e403397a58412fb42d1c3f967685244
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2022-10-29 19:32:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ st.title('GPT-3 Instruct')
 
 st.text('This GPT-3 model is the Davinci-engine')
 prompt_text = st.text_input(label="Add here phrase, which you want to be completed", value="Add here few words")
-response = openai.Completion.create(engine="text-davinci-002", prompt=Escriba un artículo para blog sobre el tema indicado, max_tokens=3000)
+response = openai.Completion(engine="text-davinci-002", prompt=Escriba un artículo para blog sobre el tema indicado, max_tokens=3000)
 st.text('Artículo:')
 st.text(response["choices"][0]["text"])
